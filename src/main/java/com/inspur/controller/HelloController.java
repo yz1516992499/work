1439fe082863810956f6b9a7ce9001b9fef3a4d2
yongzheng <yongzheng@inspur.com>
2022-07-13 21:33:12 +0800

    第二次提交

--- src/main/java/com/inspur/controller/HelloController.java
+++ src/main/java/com/inspur/controller/HelloController.java
@@ -3,12 +3,14 @@ package com.inspur.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "HelloWorld";
     }
+    @RequestMapping("/bye")
+    public String bye(){return "ByeBye";}
 }
